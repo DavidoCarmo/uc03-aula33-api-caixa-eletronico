c1e0047ec39094efdbc0e91b569aeefb01caf6b5
Davi do Carmo <davigcarmo@gmail.com>
2024-01-26 21:38:18 -0300

    criada a rota de saldo

--- src/index.js
+++ src/index.js
@@ -6,58 +6,104 @@ const { randomUUID } = require('crypto')
 const app = express()
 const port = 5000
 
+//==============================================================
+
 app.use(express.json())
 let contas = [
     {
         "id": randomUUID,
-    "nome": "Davi",
-    "numero_conta":"12345-6",
-    "agencia":"1234",
-    "saldo": 0,
-},
-{
-    "id": randomUUID,
-    "nome": "Murilo",
-    "numero_conta":"5432-1",
-    "agencia":"1234",
-    "saldo": 0,
-},
-{
-    "id": randomUUID,
-    "nome": "Eberto",
-    "numero_conta":"1313-0",
-    "agencia":"1234",
-    "saldo": 0,
-},
-];
+        "nome": "Davi",
+        "numero_conta": "12345-6",
+        "agencia": "1234",
+        "saldo": 0,
+    },
+
+    {
+        "id": randomUUID,
+        "nome": "Murilo",
+        "numero_conta": "5432-1",
+        "agencia": "1234",
+        "saldo": 0,
+    },
+
+    {
+        "id": randomUUID,
+        "nome": "Eberto",
+        "numero_conta": "1313-0",
+        "agencia": "1234",
+        "saldo": 0,
+    }];
+
+
 
 app.get("/status", (request, response) => {
-  response.send({ "status": "ok" })
+    response.send({ "status": "ok" })
 })
 
 
-//Rota Boas Vindas 
-app.get('/conta/:numero_conta',(request,response) =>{
-    const {numero_conta} = request.params;
-    
-    const contaEncontrada = contas.find(
-        (conta) => conta.numero_conta === numero_conta )
-        
-        
-        if(contaEncontrada) {
-            //retornar os dados do conta
-            response.send({
-                "nome": contaEncontrada.nome,
-                "numero_conta":contaEncontrada.numero_conta,
-                "saldo":contaEncontrada.saldo
-            })
-        } else {
-            response.status(404).send({
-                "error":"conta nao encontrada"
-            })
-        }
 
-    })
-    app.listen(port, () => {
-      console.log(`API rodando na porta ${port}`)
-    })
+//Rota Boas Vindas 
+app.get('/conta/:numero_conta', (request, response) => {
+    const { numero_conta } = request.params;
+
+
+
+
+    const contaEncontrada = contas.find(
+        (conta) => conta.numero_conta === numero_conta)
+
+
+
+
+    if (contaEncontrada) {
+        //retornar os dados do conta
+        response.send({
+            "nome": contaEncontrada.nome,
+            "numero_conta": contaEncontrada.numero_conta,
+            "saldo": contaEncontrada.saldo
+        })
+    } else {
+        response.status(404).send({
+            "error": "conta nao encontrada"
+        })
+    }
+
+})
+
+app.get("/status", (request, response) => {
+    response.send({ "status": "ok" })
+})
+
+
+
+//-----------------------------------------------------------------------
+
+//Rota Saldo
+app.get('/conta/:numero_conta/saldo', (request, response) => {
+    const { numero_conta } = request.params;
+
+    const contaEncontrada = contas.find(
+        (conta) => conta.numero_conta === numero_conta)
+
+
+    if (contaEncontrada) {
+        //retornar os dados do conta
+        response.send({
+            "saldo": contaEncontrada.saldo
+        })
+    } else {
+        response.status(404).send({
+            "error": "conta nao encontrada"
+        })
+    }
+
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`API rodando na porta ${port}`)
+})
