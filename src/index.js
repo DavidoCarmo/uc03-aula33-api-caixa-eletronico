edd42cc0aba71939b4748f71499dea6d3a00060b
Davi do Carmo <davigcarmo@gmail.com>
2024-01-30 21:55:35 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,143 +1,118 @@
-
-const { request } = require('http')
-const express = require('express')
-const { randomUUID } = require('crypto')
+import express from 'express'
+import { randomUUID } from 'crypto'
 
 const app = express()
 const port = 5000
 
-//==============================================================
-
 app.use(express.json())
+
 let contas = [
-    {
-        "id": randomUUID,
-        "nome": "Davi",
-        "numero_conta": "12345-6",
-        "agencia": "1234",
-        "saldo": 0,
-    },
-
-    {
-        "id": randomUUID,
-        "nome": "Murilo",
-        "numero_conta": "5432-1",
-        "agencia": "1234",
-        "saldo": 0,
-    },
-
-    {
-        "id": randomUUID,
-        "nome": "Eberto",
-        "numero_conta": "1313-0",
-        "agencia": "1234",
-        "saldo": 0,
-    }];
-
-
+  {
+    "id": randomUUID(),
+    "nome": "Davi",
+    "numero_conta": "12345-6",
+    "agencia": "1234",
+    "saldo": 0,
+  },
+  {
+    "id": randomUUID(),
+    "nome": "Murilo",
+    "numero_conta": "5432-1",
+    "agencia": "1234",
+    "saldo": 0,
+  },
+  {
+    "id": randomUUID(),
+    "nome": "Eberton",
+    "numero_conta": "1313-0",
+    "agencia": "1234",
+    "saldo": 0,
+  },
+]
 
 app.get("/status", (request, response) => {
-    response.send({ "status": "ok" })
+  response.send({ "status": "ok" })
 })
 
-
-
-//Rota Boas Vindas 
+// Rota Boas Vindas 
 app.get('/conta/:numero_conta', (request, response) => {
-    const { numero_conta } = request.params;
+  const { numero_conta } = request.params;
 
+  const contaEncontrada = contas.find(
+    (conta) => conta.numero_conta === numero_conta
+  )
 
-
-
-    const contaEncontrada = contas.find(
-        (conta) => conta.numero_conta === numero_conta)
-
-
-
-
-    if (contaEncontrada) {
-        //retornar os dados do conta
-        response.send({
-            "nome": contaEncontrada.nome,
-            "numero_conta": contaEncontrada.numero_conta,
-            "saldo": contaEncontrada.saldo
-        })
-    } else {
-        response.status(404).send({
-            "error": "conta nao encontrada"
-        })
-    }
-
-})
-
-app.get("/status", (request, response) => {
-    response.send({ "status": "ok" })
-})
-
-
-
-//-----------------------------------------------------------------------
-
-//Rota Saldo
-app.get('/conta/:numero_conta/saldo', (request, response) => {
-    const { numero_conta } = request.params;
-
-    const contaEncontrada = contas.find(
-        (conta) => conta.numero_conta === numero_conta)
-
-
-    if (contaEncontrada) {
-        //retornar os dados do conta
-        response.send({
-            "saldo": contaEncontrada.saldo
-        })
-    } else {
-        response.status(404).send({
-            "error": "conta nao encontrada"
-        })
-    }
-
-})
-
-
-
-//=============================================================================
-
-//Rota deposito
-
-app.post('/conta/:numero_conta/deposito',(request,response) =>{
-    const {numero_conta} = request.params;
-
-    const {valor, tipo} = request.body;
-
-
-    //validar se a conta existe 
-    const contaEncontrada = contas.find(
-        (conta) => conta.numero_conta === numero_conta)
-    
-if(!contaEncontrada){
+  if (contaEncontrada) {
+    // retornar os dados da conta
     response.send({
-        "error": "Contra nao encontrada"
+      "nome": contaEncontrada.nome,
+      "numero_conta": contaEncontrada.numero_conta,
+      "saldo": contaEncontrada.saldo
     })
-}
-    //validar se o valor e positivo 
-    if (!valor || valor <= 0){
-        response.send({
-            "error": "Valor Invalido"})
-    //validar qual o tipo de deposito (dinheiro e cheque)
-    if(!tipo){response.send({
-        "error": "tipo invalido"
-      })
-    }else if (tipo.toUpperCase() === 'DINHEIRO'){
-        //TODO validar se e inteiro
-    }
-}})
+  } else {
+    response.status(404).send({
+      "error": "Conta não encontrada"
+    })
+  }
+});
 
 
+// Rota Saldo
+app.get('/conta/:numero_conta/saldo', (request, response) => {
+  const { numero_conta } = request.params;
 
+  const contaEncontrada = contas.find(
+    (conta) => conta.numero_conta === numero_conta
+  )
+
+  if (contaEncontrada) {
+    // retornar os dados da conta
+    response.send({
+      "saldo": contaEncontrada.saldo
+    })
+  } else {
+    response.status(404).send({
+      "error": "Conta não encontrada"
+    })
+  }
+});
+
+// Rota depósito
+app.post('/conta/:numero_conta/deposito', (request, response) => {
+  const { numero_conta } = request.params;
+
+  const { valor, tipo } = request.body;
+
+  // validar se a conta existe
+  const contaEncontrada = contas.find(
+    (conta) => conta.numero_conta === numero_conta
+  )
+
+  if (!contaEncontrada) {
+    response.send({
+      "error": "Conta não encontrada"
+    })
+  }
+
+  // validar se o valor é positivo
+  if (!valor || valor <= 0) {
+    response.send({
+      "error": "Valor inválido"
+    })
+  }
+
+  // validar qual o tipo de depósito (DINHEIRO ou CHEQUE)
+  if (!tipo || typeof tipo !== String) {
+    response.send({
+      "error": "Tipo inválido"
+    })
+  } else if (tipo.toUpperCase() === 'DINHEIRO'){
+    // TODO - Validar se é inteiro
+  }
+})
 
 
 
 app.listen(port, () => {
-    console.log(`API rodando na porta ${port}`)
+  console.log(`API rodando na porta ${port}`)
 })
