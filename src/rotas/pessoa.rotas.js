ca65ab1690123998817965a54305609b6207f39f
Davi do Carmo <davigcarmo@gmail.com>
2024-02-06 21:57:33 -0300

    nova rotas: de login de pessoa e rota de atualização

--- src/rotas/pessoa.rotas.js
+++ src/rotas/pessoa.rotas.js
@@ -1,11 +1,53 @@
-import { Router} from "express";
+import { Router, response} from "express";
 import database from "../database/index.js";
 import {randomUUID} from "node:crypto"
+import { request } from "node:http";
+import { authorizeMiddleware } from '../middlewares/auth.middleware.js'
+import jwtUtils from '../utils/jwt.utils.js'
+
+
 const router = Router()
+
+
+router.post("/login", async (request, response) => {
+  const { usuario, senha } = request.body
+
+  const db = database.getDB()
+
+    const pessoa = await db.oneOrNone(
+        `SELECT * 
+         FROM banco.PESSOAS 
+         WHERE banco.PESSOAS.deleted_at is null 
+           AND banco.PESSOAS.usuario = $1`,
+        [usuario])
+
+  if (!pessoa) {
+    response.send({ error: "Usuário inválido" })
+  }
+
+  if (pessoa.senha === senha) {
+    // gera o JWT - JSON Web Token
+    const payload = {
+      pessoa_id: pessoa.pessoa_id,
+      privilegio: pessoa.privilegio
+    }
+    const token = jwtUtils.generateToken(payload)
+
+    response.send({
+      message: "Usuário autenticado",
+      jwt: token
+    })
+  } else {
+    response.send({ error: "Usuário inválido" })
+  }
+
+})
+
+
 
 // Cadastro de pessoas
 
-router.post('/cadastro',async(request,response) =>{
+router.post('cadastrar',async(request,response) =>{
     const {nome_completo, cpf , usuario,senha} = request.body
     console.log (nome_completo,cpf,usuario,senha)
     const db = database.getDB()
@@ -33,5 +75,17 @@ const pessoa = await db.one(
 response.send({message:"Cadastro de pessoa criado com sucesso",
 pessoa_id:pessoa.pessoa_id
 })
+})
+
+router.patch('/:pessoa_id/atualizar',authorizeMiddleware, async(request,response)=> {
+    //obter do body os campos para atualizar
+    const {nome_completo, cpf , usuario,senha, privilegio} = request.body
+    //validar  se é a propria pessoa que esta logada fazendo a atualização
+
+    //senão atualizar se a pessoa e um bancario ou admin
+
+    // vai verificar se existe uma conta com este id
+
+    //atualizar o registro
 })
 export default {router}
